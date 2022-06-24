bb27104a54f956977668b4fb1f37f84192a39ff7
robert-kn <robertndungu90@gmail.com>
2022-06-24 14:22:28 +0100

    recently updated

--- C/ANSI C/Exercise1-21/entab.c
+++ C/ANSI C/Exercise1-21/entab.c
@@ -18,7 +18,7 @@ int main(){
     nb = 0;                         /* # of blanks necessary */
     nt = 0;                         /* # of tabs necessary */
 
-    for(pos = 1; (c = getchar()) != EOF; ++pos){
+    for(pos = 1; (c = getchar()) != EOF; ++pos)
         if(c == ' '){
             if(pos % TABINC != 0){
                 ++nb;              /* increment # of blanks */
@@ -47,5 +47,5 @@ int main(){
             else if(c == '\t')
                 pos = pos + (TABINC - (pos - 1) % TABINC) - 1;
         }
-    }
+    
 }
