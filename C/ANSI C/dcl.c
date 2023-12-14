1506b60e83b63cd0162d3af8e21956e501553562
robert-kn <robertndungu90@gmail.com>
2023-12-14 22:29:24 +0000

    more code

--- C/ANSI C/dcl.c
+++ C/ANSI C/dcl.c
@@ -29,6 +29,28 @@ main() /* convert declaration to words */
     return 0;
 }
 
+/* undcl: convert word descriptions to declarations */
+// main()
+// {
+//     int type;
+//     char temp[MAXTOKEN];
+//     while (gettoken() != EOF) {
+//         strcpy(out, token);
+//         while ((type = gettoken()) != '\n')
+//             if (type == PARENS || type == BRACKETS)
+//                 strcat(out, token);
+//             else if (type == '*') {
+//                 sprintf(temp, "(*%s)", out);
+//                 strcpy(out, temp);
+//             } else if (type == NAME) {
+//                 sprintf(temp, "%s %s", token, out);
+//                 strcpy(out, temp);
+//             } else
+//                 printf("invalid input at %s\n", token);
+//     }
+//     return 0;
+// }
+
 int gettoken(void) /* return next token */
 {
     int c, getch(void);
