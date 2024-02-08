25eb2ee1544394354388807db2ac273c1f3d280e
robertkn <robertndungu90@gmail.com>
2024-02-08 22:51:05 +0000

    more code

---
+++ C/Computer systems/p11.c
@@ -0,0 +1,18 @@
+/**
+ * Suppose you work as a truck driver, and you have been hired to carry a load of potatoes from Boise, Idaho, 
+ * to Minneapolis, Minnesota, a total distance of 2,500 kilometers. You estimate you can average 100 km/hr 
+ * driving within the speed limits, requiring a total of 25 hours for the trip.
+ * 
+ * You hear on the news that Montana has just abolished its speed limit, which constitutes 1,500 km of the trip. 
+ * Your truck can travel at 150 km/hr. What will be your speedup for the trip?
+ * 
+ * You can buy a new turbocharger for your truck at www.fasttrucks.com. They stock a variety of models, but the 
+ * faster you want to go, the more it will cost. How fast must you travel through Montana to get an overall speedup 
+ * for your trip of 1.67×?
+ * 
+ * amdahls law: s = 1/(1-alpha+alpha/k)
+ * 
+ * where alpha is the initial value and k is the new value
+ * 
+ * screenshot with answers in this folder
+*/
---
+++ C/Computer systems/p12.c
@@ -0,0 +1,8 @@
+/**
+ * The marketing department at your company has promised your customers that the next software release will show a 
+ * 2× performance improvement. You have been assigned the task of delivering on that promise. You have determined 
+ * that only 80% of the system can be improved. How much (i.e., what value of k) would you need to improve this part 
+ * to meet the overall performance target?
+ * 
+ * screenshot with answers in this folder
+*/
