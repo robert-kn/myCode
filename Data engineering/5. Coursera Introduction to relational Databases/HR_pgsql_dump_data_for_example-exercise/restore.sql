40f9f60572500033867ffee0b833c5a86bef9fc1
robert-kn <robertndungu90@gmail.com>
2025-01-11 19:14:30 +0000

    more code

---
+++ Data engineering/5. Coursera Introduction to relational Databases/HR_pgsql_dump_data_for_example-exercise/restore.sql
@@ -0,0 +1,84 @@
+--
+-- NOTE:
+--
+-- File paths need to be edited. Search for $$PATH$$ and
+-- replace it with the path to the directory containing
+-- the extracted data files.
+--
+--
+-- PostgreSQL database dump
+--
+
+-- Dumped from database version 13.2
+-- Dumped by pg_dump version 13.1
+
+SET statement_timeout = 0;
+SET lock_timeout = 0;
+SET idle_in_transaction_session_timeout = 0;
+SET client_encoding = 'UTF8';
+SET standard_conforming_strings = on;
+SELECT pg_catalog.set_config('search_path', '', false);
+SET check_function_bodies = false;
+SET xmloption = content;
+SET client_min_messages = warning;
+SET row_security = off;
+
+DROP DATABASE "HR";
+--
+-- Name: HR; Type: DATABASE; Schema: -; Owner: postgres
+--
+
+CREATE DATABASE "HR" WITH TEMPLATE = template0 ENCODING = 'UTF8' LOCALE = 'en_US.UTF-8';
+
+
+ALTER DATABASE "HR" OWNER TO postgres;
+
+\connect "HR"
+
+SET statement_timeout = 0;
+SET lock_timeout = 0;
+SET idle_in_transaction_session_timeout = 0;
+SET client_encoding = 'UTF8';
+SET standard_conforming_strings = on;
+SELECT pg_catalog.set_config('search_path', '', false);
+SET check_function_bodies = false;
+SET xmloption = content;
+SET client_min_messages = warning;
+SET row_security = off;
+
+--
+-- Data for Name: locations; Type: TABLE DATA; Schema: public; Owner: postgres
+--
+
+COPY public.locations (location_id, street_address, postal_code, city, state_province, country_id) FROM stdin;
+\.
+COPY public.locations (location_id, street_address, postal_code, city, state_province, country_id) FROM '$$PATH$$/3270.dat';
+
+--
+-- Data for Name: departments; Type: TABLE DATA; Schema: public; Owner: postgres
+--
+
+COPY public.departments (department_id, department_name, manager_id, location_id) FROM stdin;
+\.
+COPY public.departments (department_id, department_name, manager_id, location_id) FROM '$$PATH$$/3267.dat';
+
+--
+-- Data for Name: jobs; Type: TABLE DATA; Schema: public; Owner: postgres
+--
+
+COPY public.jobs (job_id, job_title, min_salary, max_salary) FROM stdin;
+\.
+COPY public.jobs (job_id, job_title, min_salary, max_salary) FROM '$$PATH$$/3269.dat';
+
+--
+-- Data for Name: employees; Type: TABLE DATA; Schema: public; Owner: postgres
+--
+
+COPY public.employees (employee_id, first_name, last_name, email, phone_number, hire_date, job_id, salary, commission_pct, manager_id, department_id) FROM stdin;
+\.
+COPY public.employees (employee_id, first_name, last_name, email, phone_number, hire_date, job_id, salary, commission_pct, manager_id, department_id) FROM '$$PATH$$/3268.dat';
+
+--
+-- PostgreSQL database dump complete
+--
+
